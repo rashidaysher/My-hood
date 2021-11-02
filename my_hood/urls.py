74198fa684103acdfb4fadc0ad4e51f843571df3
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-11-02 23:42:56 +0300

    defining the paths

--- my_hood/urls.py
+++ my_hood/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path, include
 from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
