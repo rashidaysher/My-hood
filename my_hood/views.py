7b7ac8059745a3af77b3fec3fd1a78e1b12bf6eb
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-11-02 23:33:13 +0300

    defining the signup function

--- my_hood/views.py
+++ my_hood/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SignupForm, BusinessForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood, Profile, Business, Post
+from .forms import UpdateProfileForm, NeighbourHoodForm, PostForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
