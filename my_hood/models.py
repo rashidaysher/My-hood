0aff29bdaee8cf2a8626fca2046c81145b68724c
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-11-02 23:51:42 +0300

    defing the post model

--- my_hood/models.py
+++ my_hood/models.py
@@ -61,4 +61,14 @@ class Business(models.Model):
         self.save()
 
     def delete_business(self):
-        self.delete()         
+        self.delete()  
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all() 
+
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='hood_post')                  
