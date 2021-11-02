2f489c26b8e31a4ad63d6dd68f3e5775e8b286cb
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-11-02 23:33:35 +0300

    defining the signup function

--- my_hood/views.py
+++ my_hood/views.py
@@ -11,3 +11,14 @@ from django.contrib.auth.models import User
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')    
