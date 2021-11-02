cf461ceea40faa0c635297886d991c8d5b1c3363
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-11-02 23:43:14 +0300

    defining the paths

--- my_hood/urls.py
+++ my_hood/urls.py
@@ -10,3 +10,10 @@ urlpatterns = [
     path('new-hood/', views.create_hood, name='new-hood'),
     path('profile/<username>', views.profile, name='profile'),
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
+]
