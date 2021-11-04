f783f35f45843582fa26a357b0cbc4caee5a2e36
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-11-04 13:11:35 +0300

    tests

--- my_hood/tests.py
+++ my_hood/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from my_hood.models import *
+
+# Create your tests here.
+
+
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create_superuser(
+            username='developer',
+            password='password'
+        )
+        self.neighborhood = NeighbourHood(
+            name='Test Neighbourhood', location=self.location, occupants_count=100, admin_id=self.admin.id)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighborhood, NeighbourHood))
+
+    def test_save_method(self):
+        self.neighbourhood.create_neigborhood()
+        neighborhoods = NeighbourHood.objects.all()
+        self.assertTrue(len(neighborhoods) > 0)
+
+    def test_delete_method(self):
+        self.neighborhood.create_neigborhood()
+        self.neighborhood.delete()
+        neighborhoods = NeighbourHood.objects.all()
+        self.assertTrue(len(neighborhoods) == 0)
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='dev-kev',
+            password='password'
+        )
+        self.post = Post(title='Test Post', content='Test Content',
+                         location=self.location, category=self.category, user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_method(self):
+        self.post.save()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+    def test_delete_method(self):
+        self.post.save()
+        self.post.delete_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) == 0)
